e9d53f6125217d556bd55738b9496a894a09a6cc
Kevin Castejon <castejonkevin@gmail.com>
2026-01-03 17:58:16 +0100

    initial commit

---
+++ LinkedRGB/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedRGB
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LinkedRGB/Permissions/BluetoothScanPermission.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedRGB.Permissions;
+
+public class BluetoothScanPermission : Microsoft.Maui.ApplicationModel.Permissions.BasePlatformPermission
+{
+#if ANDROID
+    public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
+        new[]
+        {
+            ("android.permission.BLUETOOTH_SCAN", true),
+            ("android.permission.BLUETOOTH_CONNECT", true),
+            ("android.permission.ACCESS_FINE_LOCATION", true),
+        };
+#endif
+}
